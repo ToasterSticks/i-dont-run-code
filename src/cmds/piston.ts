19fca411b0c986ab6b2b77a2e493ce91ab00f159
ToastOnAStick <matt.zhao.18@gmail.com>
2022-08-24 10:33:50 -0400

    feat: mobile-output

--- src/cmds/piston.ts
+++ src/cmds/piston.ts
@@ -12,7 +12,7 @@ import {
 } from 'discord-api-types/v10';
 
 import { PistonExecuteData, PistonReponse } from '../types';
-import { getModalValue, getOption, supportedMarkdown, supportedRuntimes } from '../util';
+import { API_BASE, getModalValue, getOption, supportedMarkdown, supportedRuntimes } from '../util';
 
 export const command: Command<ApplicationCommandType.ChatInput> = {
 	name: 'piston',
@@ -30,6 +30,11 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 			type: ApplicationCommandOptionType.Boolean,
 		},
 		{
+			name: 'mobile-output',
+			description: 'Whether to send the source contents as text',
+			type: ApplicationCommandOptionType.Boolean,
+		},
+		{
 			name: 'hide',
 			description: 'Whether to hide the response',
 			type: ApplicationCommandOptionType.Boolean,
@@ -38,6 +43,7 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 	handler: async ({ data: { options } }) => {
 		const language = getOption<string>(options, 'language')!.toLowerCase();
 		const file = getOption<boolean>(options, 'file') || '';
+		const mobile = getOption<boolean>(options, 'mobile-output') || '';
 		const hide = getOption<boolean>(options, 'hide') || '';
 
 		if (![...supportedRuntimes.languages, ...supportedRuntimes.aliases].includes(language))
@@ -52,7 +58,7 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 		return {
 			type: InteractionResponseType.Modal,
 			data: {
-				custom_id: `piston:${language}:${file}:${hide}`,
+				custom_id: `piston:${language}:${file}:${mobile}:${hide}`,
 				title: `Execute ${supportedMarkdown[language] ?? language} program`,
 				components: [
 					{
@@ -86,7 +92,7 @@ export const command: Command<ApplicationCommandType.ChatInput> = {
 	modal: async (interaction) => {
 		currentEvent.waitUntil(followUp(interaction));
 
-		const hide = interaction.data.custom_id.split(':')[3];
+		const hide = interaction.data.custom_id.split(':')[4];
 
 		return {
 			type: InteractionResponseType.DeferredChannelMessageWithSource,
@@ -102,7 +108,7 @@ const followUp = async ({ data, token }: APIModalSubmitInteraction) => {
 		.split(',')
 		.map((arg) => arg.trim());
 
-	const [, language, file] = data.custom_id.split(':');
+	const [, language, file, mobile, hide] = data.custom_id.split(':');
 
 	const result = await retryUntilSuccess({
 		language,
@@ -113,6 +119,7 @@ const followUp = async ({ data, token }: APIModalSubmitInteraction) => {
 	});
 
 	let body: FormData | string;
+	let followUpBody: FormData | undefined;
 
 	if ('message' in result) body = JSON.stringify({ content: result.message });
 	else {
@@ -125,26 +132,35 @@ const followUp = async ({ data, token }: APIModalSubmitInteraction) => {
 			supportedMarkdown[language] ?? language
 		} (${version}) program; output is below`;
 
-		const charsRemaining = 2000 - 7 - reply.length;
-
-		if (!file)
-			reply += `\`\`\`\n${
-				joinedOutput.length > charsRemaining
-					? joinedOutput.slice(0, charsRemaining - 3) + '[…]'
-					: joinedOutput || ' '
-			}\`\`\``;
-		else files.push({ name: 'output.txt', data: joinedOutput });
+		if (file) files.push({ name: 'output.txt', data: joinedOutput });
+		else reply += truncateOutputWithCodeblock(joinedOutput, reply.length);
 
 		files.push({ name: language + (supportedMarkdown[language] ? '' : '.txt'), data: code });
 		if (stdin) files.push({ name: 'stdin.txt', data: stdin });
 
-		body = formDataResponse({ content: reply, files });
+		body = formDataResponse({ content: reply, files: mobile ? (file ? [files[0]] : []) : files });
+
+		if (mobile) {
+			followUpBody = formDataResponse({
+				content: truncateOutputWithCodeblock(code, 0, language),
+				files: files.slice(file ? 2 : 1),
+				flags: hide ? MessageFlags.Ephemeral : 0,
+			});
+		}
 	}
 
-	return fetch('https://discord.com/api/v10' + Routes.webhook(CLIENT_ID, token), {
+	await fetch(API_BASE + Routes.webhook(CLIENT_ID, token), {
 		method: 'POST',
 		body,
 	});
+
+	if (followUpBody)
+		fetch(API_BASE + Routes.webhook(CLIENT_ID, token), {
+			method: 'POST',
+			body: followUpBody,
+		});
+
+	return;
 };
 
 const getPistonResponse = (data: PistonExecuteData) =>
@@ -172,4 +188,12 @@ const formDataResponse = (data: RESTPostAPIInteractionFollowupJSONBody & { files
 	formData.append('payload_json', JSON.stringify(data));
 
 	return formData;
+};
+
+const truncateOutputWithCodeblock = (str: string, charCountUsed = 0, lang = '') => {
+	const charsRemaining = 1993 - charCountUsed - lang.length;
+
+	return `\`\`\`${lang}\n${
+		str.length > charsRemaining ? str.slice(0, charsRemaining - 3) + '[…]' : str || ' '
+	}\`\`\``;
 };
