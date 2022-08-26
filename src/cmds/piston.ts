f4ec9b440f6d1229dc69e58df2729f62bfc67a7b
ToastOnAStick <matt.zhao.18@gmail.com>
2022-08-26 14:58:44 -0400

    fix: remove default new line in response

--- src/cmds/piston.ts
+++ src/cmds/piston.ts
@@ -136,7 +136,7 @@ const followUp = async ({ data, token }: APIModalSubmitInteraction) => {
 
 		const files: File[] = [];
 
-		const joinedOutput = [compile?.output, run.output].join('\n');
+		const joinedOutput = [compile?.output, run.output].join('\n').slice(0, -1);
 		const trimmed = joinedOutput.trim();
 		let reply = `Executed your ${supportedMarkdown[language] ?? language} (${version}) program; ${
 			joinedOutput ? 'output is below' : 'no output received'
