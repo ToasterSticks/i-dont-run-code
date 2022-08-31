4a8079858e9d3c29c87dd6787511f3f8eac573fe
ToastOnAStick <matt.zhao.18@gmail.com>
2022-08-31 10:00:12 -0400

    fix: unused type

--- src/http-interactions/interaction.ts
+++ src/http-interactions/interaction.ts
@@ -56,7 +56,6 @@ export const interaction = ({
 }: {
 	publicKey: string;
 	commands: CommandStore;
-	components?: { [key: string]: InteractionHandler };
 }) => {
 	return async (request: Request): Promise<Response> => {
 		const validateRequest = makeValidator({ publicKey });
